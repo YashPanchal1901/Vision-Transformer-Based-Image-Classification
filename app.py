1920f01e4ab080b424dec0feb11bd43e227c0df9
Yash Panchal <147432437+YashPanchal1901@users.noreply.github.com>
2024-09-18 12:43:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,102 @@
+import streamlit as st
+from PIL import Image
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+import requests
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# Load the pre-trained Vision Transformer model and feature extractor
+model_name = "google/vit-base-patch16-224"
+feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
+model = ViTForImageClassification.from_pretrained(model_name)
+
+# API key for the nutrition information
+api_key = '7dT5MrExu46cdcZXfYkyaQ==6ZqQQDMF3LOOdTgY'
+
+
+def identify_image(image):
+    """Identify the food item in the image."""
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class_idx = logits.argmax(-1).item()
+    predicted_label = model.config.id2label[predicted_class_idx]
+    food_name = predicted_label.split(',')[0]
+    return food_name
+
+
+def get_calories(food_name):
+    """Get the calorie information of the identified food item."""
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(food_name)
+    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+    if response.status_code == requests.codes.ok:
+        nutrition_info = response.json()
+    else:
+        nutrition_info = {"Error": response.status_code, "Message": response.text}
+    return nutrition_info
+
+
+def format_nutrition_info(nutrition_info):
+    """Format the nutritional information into an HTML table."""
+    if "Error" in nutrition_info:
+        return f"Error: {nutrition_info['Error']} - {nutrition_info['Message']}"
+
+    if len(nutrition_info) == 0:
+        return "No nutritional information found."
+
+    nutrition_data = nutrition_info[0]
+    table = f"""
+    <table border="1" style="width: 100%; border-collapse: collapse;">
+        <tr><th colspan="4" style="text-align: center;"><b>Nutrition Facts</b></th></tr>
+        <tr><td colspan="4" style="text-align: center;"><b>Food Name: {nutrition_data['name']}</b></td></tr>
+        <tr>
+            <td style="text-align: left;"><b>Calories</b></td><td style="text-align: right;">{nutrition_data['calories']}</td>
+            <td style="text-align: left;"><b>Serving Size (g)</b></td><td style="text-align: right;">{nutrition_data['serving_size_g']}</td>
+        </tr>
+        <tr>
+            <td style="text-align: left;"><b>Total Fat (g)</b></td><td style="text-align: right;">{nutrition_data['fat_total_g']}</td>
+            <td style="text-align: left;"><b>Saturated Fat (g)</b></td><td style="text-align: right;">{nutrition_data['fat_saturated_g']}</td>
+        </tr>
+        <tr>
+            <td style="text-align: left;"><b>Protein (g)</b></td><td style="text-align: right;">{nutrition_data['protein_g']}</td>
+            <td style="text-align: left;"><b>Sodium (mg)</b></td><td style="text-align: right;">{nutrition_data['sodium_mg']}</td>
+        </tr>
+        <tr>
+            <td style="text-align: left;"><b>Potassium (mg)</b></td><td style="text-align: right;">{nutrition_data['potassium_mg']}</td>
+            <td style="text-align: left;"><b>Cholesterol (mg)</b></td><td style="text-align: right;">{nutrition_data['cholesterol_mg']}</td>
+        </tr>
+        <tr>
+            <td style="text-align: left;"><b>Total Carbohydrates (g)</b></td><td style="text-align: right;">{nutrition_data['carbohydrates_total_g']}</td>
+            <td style="text-align: left;"><b>Fiber (g)</b></td><td style="text-align: right;">{nutrition_data['fiber_g']}</td>
+        </tr>
+        <tr>
+            <td style="text-align: left;"><b>Sugar (g)</b></td><td style="text-align: right;">{nutrition_data['sugar_g']}</td>
+            <td></td><td></td>
+        </tr>
+    </table>
+    """
+    return table
+
+
+
+st.title('Food Nutrition Information')
+st.write('Upload an image of a food item to get its nutritional information.')
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+
+    st.write("Identifying food item...")
+    food_name = identify_image(image)
+    st.write(f"Food Item: {food_name}")
+
+    st.write("Fetching nutritional information...")
+    nutrition_info = get_calories(food_name)
+    nutrition_table = format_nutrition_info(nutrition_info)
+    st.write(nutrition_table, unsafe_allow_html=True)
+
+
